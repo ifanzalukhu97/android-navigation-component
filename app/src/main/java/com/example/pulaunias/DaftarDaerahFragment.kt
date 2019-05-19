bfb6a9767115310612068fa2c7010c2a2a09bf96
ifanzalukhu97 <ivan.zalukhu97@gmail.com>
2019-05-20 00:57:15 +0700

    Implementation Safe Args

--- app/src/main/java/com/example/pulaunias/DaftarDaerahFragment.kt
+++ app/src/main/java/com/example/pulaunias/DaftarDaerahFragment.kt
@@ -6,16 +6,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_daftar_daerah.view.*
 
 class DaftarDaerahFragment : Fragment() {
+
+    companion object {
+        const val KOTA_GUNUNGSITOLI = "gunungsitoli"
+        const val NIAS_UTARA = "nias_utara"
+        const val NIAS_BARAT = "nias_barat"
+        const val NIAS_SELATAN = "nias_selatan"
+        const val NIAS = "nias"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daftar_daerah, container, false)
+        val layoutView = inflater.inflate(R.layout.fragment_daftar_daerah, container, false)
+
+        layoutView.kota_gununsitoli.setOnClickListener { showDetailCity(KOTA_GUNUNGSITOLI, it) }
+        layoutView.kab_nias_utara.setOnClickListener { showDetailCity(NIAS_UTARA, it) }
+        layoutView.kab_nias_barat.setOnClickListener { showDetailCity(NIAS_BARAT, it) }
+        layoutView.kab_nias_selatan.setOnClickListener { showDetailCity(NIAS_SELATAN, it) }
+        layoutView.kab_nias.setOnClickListener { showDetailCity(NIAS, it) }
+
+        return layoutView
     }
 
+    private fun showDetailCity(cityName: String, view: View) {
+        view.findNavController().navigate(DaftarDaerahFragmentDirections.actionDaftarDaerahToDetailDaerah(cityName))
+    }
 
 }
--- app/src/main/java/com/example/pulaunias/DetailDaerahFragment.kt
+++ app/src/main/java/com/example/pulaunias/DetailDaerahFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_detail_daerah.view.*
 
 class DetailDaerahFragment : Fragment() {
 
@@ -14,7 +15,54 @@ class DetailDaerahFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_daerah, container, false)
+        val layoutView = inflater.inflate(R.layout.fragment_detail_daerah, container, false)
+
+        getShareArgs(layoutView)
+
+        return layoutView
+    }
+
+    private fun getShareArgs(layoutView: View) {
+        val args = DetailDaerahFragmentArgs.fromBundle(arguments!!)
+
+        when (args.cityName) {
+            DaftarDaerahFragment.KOTA_GUNUNGSITOLI -> {
+                with(layoutView) {
+                    txt_name_detail_daerah.text = getString(R.string.kota_gunungsitoli)
+                    img_logo_detail_daerah.setImageDrawable(context.getDrawable(R.drawable.logo_gunungsitoli))
+                    txt_desc_detail_daerah.text = getString(R.string.desc_kota_gunungsitoli)
+                }
+            }
+            DaftarDaerahFragment.NIAS_UTARA -> {
+                with(layoutView) {
+                    txt_name_detail_daerah.text = getString(R.string.kab_nias_utara)
+                    img_logo_detail_daerah.setImageDrawable(context.getDrawable(R.drawable.logo_nias_utara))
+                    txt_desc_detail_daerah.text = getString(R.string.desc_kab_nias_utara)
+                }
+            }
+            DaftarDaerahFragment.NIAS_BARAT -> {
+                with(layoutView) {
+                    txt_name_detail_daerah.text = getString(R.string.kab_nias_barat)
+                    img_logo_detail_daerah.setImageDrawable(context.getDrawable(R.drawable.logo_nias_barat))
+                    txt_desc_detail_daerah.text = getString(R.string.desc_kab_nias_barat)
+                }
+            }
+            DaftarDaerahFragment.NIAS_SELATAN -> {
+                with(layoutView) {
+                    txt_name_detail_daerah.text = getString(R.string.kab_nias_selatan)
+                    img_logo_detail_daerah.setImageDrawable(context.getDrawable(R.drawable.logo_nias_selatan))
+                    txt_desc_detail_daerah.text = getString(R.string.desc_kab_nias_selatan)
+                }
+            }
+            else -> {
+                with(layoutView) {
+                    txt_name_detail_daerah.text = getString(R.string.kab_nias)
+                    img_logo_detail_daerah.setImageDrawable(context.getDrawable(R.drawable.logo_nias))
+                    txt_desc_detail_daerah.text = getString(R.string.desc_kab_nias)
+                }
+            }
+        }
+
     }
 
 
